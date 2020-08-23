3e3a7612cc1a67cfea53c285461cd323633bc254
DiegoMC9 <dmartinezcapel@gmail.com>
2020-08-23 22:03:21 +0200

    opcodes finished

--- src/cpu.c
+++ src/cpu.c
@@ -27,12 +27,13 @@ uint8_t I = 0x00;
 
 // two kinds of timer . both work at 60Hz
 uint8_t delay_timer = 0;
-uint8_t beep_timer = 0;
+uint8_t sound_timer = 0;
 
 // error managing variable
 int *error;
 
-struct nibbles {
+struct nibbles
+{
   uint8_t I : 4;     // instruction type first 4 bits
   uint8_t X : 4;     // second nibble
   uint8_t Y : 4;     // third nibble
@@ -43,12 +44,14 @@ struct nibbles {
 
 uint8_t lo(uint16_t address) { return (uint8_t)address; }
 uint8_t hi(uint16_t address) { return (uint8_t)(address >> 8); }
-uint16_t fetch() {
+uint16_t fetch()
+{
   uint16_t opcode = (uint16_t)mem[PC] << 8 | mem[PC + 1];
   PC += 2;
   return opcode;
 }
-struct nibbles decode(uint16_t opcode) {
+struct nibbles decode(uint16_t opcode)
+{
   struct nibbles op_nibbles;
   op_nibbles.I = (0xF000 & opcode) >> 12;
   op_nibbles.X = (0x0F00 & opcode) >> 8;
@@ -58,30 +61,39 @@ struct nibbles decode(uint16_t opcode) {
   op_nibbles.NNN = (0x0FFF & opcode);
   return op_nibbles;
 }
-int execute(uint16_t instr) {
+int execute(uint16_t instr)
+{
   struct nibbles op_nibbles = decode(instr);
 
-  switch (op_nibbles.I) {
+  switch (op_nibbles.I)
+  {
   case 0x0: // special functions
-    if (op_nibbles.X == 0) {
-      if (op_nibbles.NN == 0xE0) {
+    if (op_nibbles.X == 0)
+    {
+      if (op_nibbles.NN == 0xE0)
+      {
         // CLEAR SCREEN
-      } else if (op_nibbles.NN == 0xEE) {
+      }
+      else if (op_nibbles.NN == 0xEE)
+      {
         // return
-        PC = pop(stack, error);
-        if (error) {
+        if (pop(stack, &PC))
+        {
           perror("invalid return value");
           exit(1);
         }
       }
-    } else {
+    }
+    else
+    {
       // 0x0NNN : CALL NNN
-      push(stack, PC, error);
-      PC = op_nibbles.NNN;
-      if (error) {
+
+      if (push(stack, PC))
+      {
         perror("StackOverflow :3");
         exit(1);
       }
+      PC = op_nibbles.NNN;
     }
     break;
   case 0x1: // inconditional jump
@@ -107,7 +119,8 @@ int execute(uint16_t instr) {
     VX += op_nibbles.NN;
     break;
   case 0x8: // logic operations and assignment
-    switch (op_nibbles.N) {
+    switch (op_nibbles.N)
+    {
     case 0x0: // Vx=Vy
       VX = VY;
       break;
@@ -147,25 +160,85 @@ int execute(uint16_t instr) {
   case 0xD: // draw(Vx,Vy,N)
     draw(VX, VY, op_nibbles.N);
     break;
+  case 0xE: // check keypresses
+    if (op_nibbles.NN == 0x9E)
+    {
+      // key X is pressed
+      PC += key() == VX;
+    }
+    else if (op_nibbles.NN == 0xA1)
+    {
+      // key X not pressed
+      PC += key() != VX;
+    }
+    break;
+  case 0xF: // memory, timers, sprites
+    switch (op_nibbles.NN)
+    {
+    case 0x07:
+      // get dalay
+      VX = delay_timer;
+      break;
+    case 0x0A:
+      // get key
+      VX = get_key();
+      break;
+    case 0x15:
+      // set delay
+      delay_timer = VX;
+      break;
+    case 0x18:
+      sound_timer = VX;
+      break;
+    case 0x1E:
+      I += VX;
+      break;
+    case 0x29:
+      I = sprite(VX);
+      break;
+    case 0x33:
+      mem[I] = VX / 100;
+      mem[I + 1] = (VX % 100) / 10;
+      mem[I + 2] = (VX % 100) % 10;
+      break;
+    case 0x55: // dump registers
+      for (int i = 0; i < op_nibbles.X; i++)
+      {
+        mem[I + i] = regs[i];
+      }
+      break;
+    case 0x65: // load registers
+      for (int i = 0; i < op_nibbles.X; i++)
+      {
+        regs[i] = mem[I + i];
+      }
+      break;
+    }
+    break;
   }
   return 0;
 }
 
-int loadFont(char *fontFile) {
+int loadFont(char *fontFile)
+{
   FILE *font = fopen(fontFile, "r");
   uint8_t val;
   int elems = -1;
   fscanf(font, "%d", val);
-  while (val != EOF && elems < 0x201) {
+  while (val != EOF && elems < 0x201)
+  {
     mem[++elems] = val;
     fscanf(font, "%d", val);
   }
 }
-int main(int argc, char *argv) {
+int main(int argc, char *argv)
+{
   loadFont("font");
-  stack = newStack(16);
-  while (1) {
+  stack = create_stack(16);
+  while (1)
+  {
     uint16_t instr = fetch();
     execute(instr);
   }
+  free_stack(stack);
 }
